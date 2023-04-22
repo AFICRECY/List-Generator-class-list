838b76198b6ed9c5eae45c0ba5e0bb9e86822fd5
Afi Nkhume-Crecy <Aficrecy@gmail.com>
2023-04-21 22:37:53 -0700

    added initial class component

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const CLI = require('./lib/cli.js');
 const cli = new CLI();
 
 cli.run();
+
+class Component {
+    constructor(render, renderInnerHTML) {
+        this.render = (err, "Child class must implement" + render())
+    }
+renderinnerHTML() => {
+for (var i = 0; i < toChildren.length; i++) {
+
+}
+}
+
+}
